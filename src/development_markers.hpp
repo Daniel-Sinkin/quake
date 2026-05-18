72bdd5c697dcca279b46526f43ff445e7b0509b8
Daniel-Sinkin <danielsinkin97@gmail.com>
2026-05-18 11:55:29 +0200

    Updated to new template

--- src/development_markers.hpp
+++ src/development_markers.hpp
@@ -1,6 +1,41 @@
+// src/development_markers.hpp
+
+#ifndef QUAKE_SRC_DEVELOPMENT_MARKERS_HPP
+#define QUAKE_SRC_DEVELOPMENT_MARKERS_HPP
+
 #pragma once
+
+#include <type_traits>
+
+#if defined(mut)
+#    error "mut marker macro is already defined"
+#endif
+
+#if defined(cpy)
+#    error "cpy marker macro is already defined"
+#endif
+
+#if defined(def)
+#    error "def marker macro is already defined"
+#endif
+
+#if defined(__clang__)
+#    define mut [[clang::annotate("mut")]]
+#    define cpy [[clang::annotate("cpy")]]
+#else
+#    define mut
+#    define cpy
+#endif
+
+#define def auto
 
 namespace quake
 {
-inline constexpr auto project_name = "quake";
+template <typename T>
+[[nodiscard]] constexpr def copy(const T& value) -> std::remove_cvref_t<T>
+{
+    return value;
+}
 }  // namespace quake
+
+#endif  // QUAKE_SRC_DEVELOPMENT_MARKERS_HPP
--- src/pch.hpp
+++ src/pch.hpp
@@ -1,5 +1,8 @@
 // src/pch.hpp
 
+#ifndef QUAKE_SRC_PCH_HPP
+#define QUAKE_SRC_PCH_HPP
+
 #pragma once
 
 #include <algorithm>      // IWYU pragma: keep
@@ -42,3 +45,5 @@
 #if defined(__ARM_NEON) || defined(__ARM_NEON__)
 #    include <arm_neon.h>  // IWYU pragma: keep
 #endif
+
+#endif  // QUAKE_SRC_PCH_HPP
